4e4588f4e10aea28601c07cc3fcdf8f0a878622c
Dyning Aida <aida.batrishya@gmail.com>
2019-11-30 22:57:42 +0700

    issue #40

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import apem 
 npm=input("Masukkan NPM ")
 paswd=input("Masukkan password akun SIAP anda ")
-speech = apem.Apem(npm, paswd) #penyelesaian issue 38
+speech = apem.Apem(npm, paswd)
 
 speech.speak()
