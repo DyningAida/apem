138fbce4dc57902b12b8437860ce5e1a17396350
Dyning Aida <aida.batrishya@gmail.com>
2019-12-07 04:57:30 +0700

    issue #43

--- apem.py
+++ apem.py
@@ -22,8 +22,8 @@ class Apem(object):
         self.driver.find_element_by_name('user_name').send_keys(self.npm)
         self.driver.find_element_by_name('user_pass').send_keys(self.paswd)
         self.driver.find_element_by_xpath('/html/body/table/tbody/tr[5]/td/table[1]/tbody/tr/td[2]/table[2]/tbody/tr[1]/td[2]/div/form/input[4]').click()
-        self.driver.find_element_by_xpath('/html/body/table/tbody/tr[5]/td/table[1]/tbody/tr/td[1]/table[2]/tbody/tr[1]/td[2]/a[5]').click() #issue 42
-        self.driver.find_element_by_xpath('/html/body/table/tbody/tr[5]/td/table[3]/tbody/tr[1]/td[2]/p[1]/table/tbody/tr/td[3]/select/option[5]').click()
+        self.driver.find_element_by_xpath('/html/body/table/tbody/tr[5]/td/table[1]/tbody/tr/td[1]/table[2]/tbody/tr[1]/td[2]/a[5]').click()
+        self.driver.find_element_by_xpath('/html/body/table/tbody/tr[5]/td/table[3]/tbody/tr[1]/td[2]/p[1]/table/tbody/tr/td[3]/select/option[5]').click() #issue 43
         self.driver.find_element_by_class_name('button').click()
     def ceknilai2(self):
         self.opsi = Options()
